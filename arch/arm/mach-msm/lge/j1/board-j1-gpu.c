5da53daf809e2def0a26dddf4c837215e13685e3
jollaman999 <admin@jollaman999.com>
2014-07-22 11:48:01 +0900

    board-j1-gpu: Fix 544MHz's bus_freq to 5 from 6
    In acpuclock-8064.c
    
    [5] = BW_MBPS(4264), /* At least 533 MHz on bus. */
    [6] = BW_MBPS(4660), /* At least 583 MHz on bus. */
    
    So I changed to 5 from 6

--- arch/arm/mach-msm/lge/j1/board-j1-gpu.c
+++ arch/arm/mach-msm/lge/j1/board-j1-gpu.c
@@ -237,7 +237,7 @@ static struct kgsl_device_platform_data kgsl_3d0_pdata = {
 #ifdef CONFIG_GPU_OVERCLOCK
  		{
 			.gpu_freq = 544000000,
-			.bus_freq = 6,
+			.bus_freq = 5,
 			.io_fraction = 0,
 		},
 #endif /* CONFIG_GPU_OVERCLOCK */
