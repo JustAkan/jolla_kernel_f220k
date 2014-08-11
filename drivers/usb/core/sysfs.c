8beb0d7d329d55faafd08ec661d0c618c2e5b887
jollaman999 <admin@jollaman999.com>
2014-08-12 04:40:22 +0900

    Revert "usb: [PATCH] match on bInterfaceNumber Part_2"
    This reverts commit 0e034476b89b477c5be35151809ccaf085794a47.

--- drivers/usb/core/sysfs.c
+++ drivers/usb/core/sysfs.c
@@ -840,7 +840,7 @@ static ssize_t show_modalias(struct device *dev,
 	alt = intf->cur_altsetting;
 
 	return sprintf(buf, "usb:v%04Xp%04Xd%04Xdc%02Xdsc%02Xdp%02X"
-			"ic%02Xisc%02Xip%02Xin%02X\n",
+			"ic%02Xisc%02Xip%02X\n",
 			le16_to_cpu(udev->descriptor.idVendor),
 			le16_to_cpu(udev->descriptor.idProduct),
 			le16_to_cpu(udev->descriptor.bcdDevice),
